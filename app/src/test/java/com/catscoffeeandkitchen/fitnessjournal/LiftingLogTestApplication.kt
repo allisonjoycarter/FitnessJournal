01910c4ce0ef00783d42414e69bbaf3f9e2ec40b
Allison Bickford <allisonjoy413@gmail.com>
2022-12-24 13:27:40 -0500

    removed bad test files

--- app/src/test/java/com/catscoffeeandkitchen/fitnessjournal/LiftingLogTestApplication.kt
+++
@@ -1,7 +0,0 @@
-package com.catscoffeeandkitchen.fitnessjournal
-
-import androidx.appcompat.app.AppCompatActivity
-import dagger.hilt.android.AndroidEntryPoint
-
-@AndroidEntryPoint
-class LiftingLogTestApplication: AppCompatActivity() { }
